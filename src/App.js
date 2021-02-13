0d55552e6e38c3f58cc215f9e6419c8ed52cba66
Manuel Duran <durbonca@gmail.com>
2021-02-13 11:34:42 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="">
+      <header className="mt-5 flex flex-col">
+        <img src={logo} className="self-center w-40" alt="logo" />
       </header>
     </div>
   );
